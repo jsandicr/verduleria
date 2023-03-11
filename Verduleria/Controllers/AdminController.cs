3b200e1407b1b4ef90918580d5e86d23dbb599e3
jsand <jsandicr16@gmail.com>
2023-03-10 21:02:10 -0600

    First Commit

---
+++ Verduleria/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Verduleria.Models;
+
+namespace Verduleria.Controllers
+{
+    public class AdminController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Producto> productoList = new List<Producto>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7024/api/Producto"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    productoList = JsonConvert.DeserializeObject<List<Producto>>(apiResponse);
+                }
+            }
+
+            return View(productoList);
+        }
+
+        public async Task<IActionResult> Promociones()
+        {
+            List<Promocion> promocionList = new List<Promocion>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7024/api/Promocion"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    promocionList = JsonConvert.DeserializeObject<List<Promocion>>(apiResponse);
+                }
+            }
+            return View(promocionList);
+        }
+    }
+}
---
+++ Verduleria/Controllers/VerduleriaController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Verduleria.Models;
+
+namespace Verduleria.Controllers
+{
+    public class VerduleriaController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<Producto> productoList = new List<Producto>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7024/api/Producto"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    productoList = JsonConvert.DeserializeObject<List<Producto>>(apiResponse);
+                }
+            }
+
+            List<Promocion> promocionList = new List<Promocion>();
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync("https://localhost:7024/api/Promocion"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    promocionList = JsonConvert.DeserializeObject<List<Promocion>>(apiResponse);
+                }
+            }
+
+            ViewData["Productos"] = productoList;
+            ViewData["Promociones"] = promocionList;
+            return View();
+        }
+    }
+}
